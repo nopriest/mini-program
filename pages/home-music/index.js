3c9e30fba0079a87245f2adc2dbc0145ea114584
hsu <frostbolt817@gmail.com>
2022-03-14 19:02:44 +0800

    搜索框
    @vant/weapp 组件搜索框 搜索详页初始化

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -15,4 +15,11 @@ Page({
 
     },
 
+    //点击搜索框
+    handleSearchClick :function(){
+        console.log("搜索框clicked!")
+        wx.navigateTo({
+            url:"/pages/detail-search/index"
+        })
+    }
 })
